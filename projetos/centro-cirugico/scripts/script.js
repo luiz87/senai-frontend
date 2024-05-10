6b84b60ad8176c5d47fc3909e59e47b535c7002b
Luiz Felipe <luizfelipe.freire@gmail.com>
2024-05-09 21:31:01 -0300

    atualizar os status

--- projetos/centro-cirugico/scripts/script.js
+++ projetos/centro-cirugico/scripts/script.js
@@ -4,9 +4,9 @@ document.getElementById('bt-novo').addEventListener('click', limparForm);
 let lista = [];
 
 let tpStatus = {
-    "Em Fila": "text-bg-info",
-    "Iniciado": "text-bg-success",
-    "Concluido": "text-bg-danger"
+    "Pré Operatório": "text-bg-info",
+    "Transferido": "text-bg-success",
+    "Recuperação": "text-bg-danger"
 }
 
 function gravar() {
