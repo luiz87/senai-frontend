718a231bdaf4f5ab99f29ed4b1785bb45aebf4a7
Luiz Felipe <luizfelipe.freire@gmail.com>
2024-05-13 20:23:19 -0300

    update: evento de clique em todos os blocos

--- projetos/jogo-memoria/js/script.js
+++ projetos/jogo-memoria/js/script.js
@@ -6,3 +6,15 @@ const tamanho = lsEmoticons.length;
 for (let i = 0; i < tamanho; i++) {
     lsEmoticons.push(lsEmoticons[i])    
 }
+
+// para cada bloco incluir o evento de click
+let lsBloco = document.getElementsByClassName("bloco");
+for (const b of lsBloco) {
+    b.addEventListener("click", ()=>{
+        mostrar(b);
+    });
+}
+
+function mostrar(b) {
+    b.innerHTML = "🤐";
+}
