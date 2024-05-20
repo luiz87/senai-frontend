fd52b0b27f3d2f5f969dff5bacda0ade058cf207
Luiz Felipe <luizfelipe.freire@gmail.com>
2024-05-20 19:47:31 -0300

    atualização

--- projetos/e-commerce/produtos.js
+++ projetos/e-commerce/produtos.js
@@ -1,1 +1,11 @@
-let produtos = [{ nome: "Banho de Mar", valor: 39.99 }, { nome: "Passeio na Floresta", valor: 39.99 }, { nome: "Trio Bom dia", valor: 39.99 }, { nome: "Banho de Mar", valor: 39.99 }, { nome: "Passeio na Floresta", valor: 39.99 }, { nome: "Trio Bom dia", valor: 39.99 }];
+let produtos = [
+    { nome: "Banho de Mar", valor: 39.00, img: "banho-de-mar.png" },
+    { nome: "Passeio na Floresta", valor: 39.00, img: "passeio-na-floresta.png" },
+    { nome: "Trio Bom dia", valor: 39.00, img: "trio-bom-dia.png" },
+    { nome: "Meus Cristais", valor: 39.00, img: "meus-cristais.png" },
+    { nome: "Campo de Lavandas", valor: 39.00, img: "campo-de-lavandas.png" },
+    { nome: "Minha Rotina", valor: 39.00, img: "minha-rotina.png" },
+    { nome: "Vida no Campo", valor: 39.00, img: "vida-no-campo.png" },
+    { nome: "Meu Momento", valor: 39.00, img: "meu-momento.png" },
+    { nome: "Flores Vivas", valor: 39.00, img: "flores-vivas.png" },
+];
